74ce5dcaf9714ddf78b7a3ce6e354280ac0d5892
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-27 12:15:08 -0300

    feat: create login endpoint

---
+++ src/main/java/com/example/login_auth_api/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.login_auth_api.controllers;
+
+import com.example.login_auth_api.domain.user.User;
+import com.example.login_auth_api.dto.LoginRequestDTO;
+import com.example.login_auth_api.dto.ResponseDTO;
+import com.example.login_auth_api.infra.security.TokenService;
+import com.example.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body ){
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        if(passwordEncoder.matches(user.getPassword(), body.password())){
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
