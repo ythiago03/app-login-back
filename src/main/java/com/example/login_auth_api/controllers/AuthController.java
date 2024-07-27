853ac5182afb7d5b10d2006aa429c0c1e098c007
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-27 12:23:50 -0300

    feat: create register endpoint

--- src/main/java/com/example/login_auth_api/controllers/AuthController.java
+++ src/main/java/com/example/login_auth_api/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.example.login_auth_api.controllers;
 
 import com.example.login_auth_api.domain.user.User;
 import com.example.login_auth_api.dto.LoginRequestDTO;
+import com.example.login_auth_api.dto.RegisterRequestDTO;
 import com.example.login_auth_api.dto.ResponseDTO;
 import com.example.login_auth_api.infra.security.TokenService;
 import com.example.login_auth_api.repositories.UserRepository;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,6 +32,23 @@ public class AuthController {
         if(passwordEncoder.matches(user.getPassword(), body.password())){
             String token = this.tokenService.generateToken(user);
             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body ){
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if(user.isEmpty()){
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
         }
         return ResponseEntity.badRequest().build();
     }
