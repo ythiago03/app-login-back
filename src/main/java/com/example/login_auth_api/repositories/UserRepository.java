9e30260e46a0d68176213ff454a69f569fe4b9d1
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-27 12:03:20 -0300

    feat: create spring security config

--- src/main/java/com/example/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
